e9efd2522832524059daed36038edfeebd164f0d
Evan Burton <Evan.Burton1@education.nsw.gov.au>
2017-10-24 14:56:37 +1100

    did a while  loop and menu system

---
+++ loops/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+            
+        {
+            string choice = "1";
+            while(choice == "1")
+            {
+                // for loop
+                Console.Writeline("for loop: ");
+
+
+
+
+
+
+
+
+
+
+
+            }
+           
+           
+                
+
+            //wait
+            Console.WriteLine("press enter to continue...");
+            Console.ReadLine();
+
+             for(int x = 1; x < 11; x = x + 1)
+            {
+                Console.WriteLine(x);
+            }
+            //wait
+            Console.WriteLine("press to start while loop.");
+            Console.ReadLine();
+
+            // while loop
+            int j = 1;
+            while(j <= 10)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+            
+           
+              
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
